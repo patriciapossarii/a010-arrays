ec136a67fa92f87a5166569abfb2b7c10bfb2e09
patriciapossarii <patricia.mag.carvalho@gmail.com>
2022-09-01 11:05:27 -0300

    Adicionando Exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+// # Exercício 1
+
+// No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+// - O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+const arrayItensNumericos= [9,6,4,3,2,7,5]
+
+// - O segundo array deve possuir apenas **strings**;
+const arrayItensStrings = ["Cafe","Leite", "Bolacha", "Biscoito","Suco","Iorgute"]
+
+// - O terceiro array deve possuir **números**, **strings** e **booleanos**.
+const arrayItensDiversos = [true, "Torrada","Pao", 6, 8]
+
+// Imprima cada um dos arrays acima, utilizando `console.log()`
+console.log(`
+--> Imprimindo Array Numérico: ${arrayItensNumericos}
+--> Imprimindo Array Strings: ${arrayItensStrings}
+--> Imprimindo Array Numeros, Strings e Booleanos: ${arrayItensDiversos}
+`)
+
